9d66d2086949e9fde3e5ff2121489eb74a4df448
Felipe Barbedo <felipebarbedo54@gmail.com>
2023-11-28 06:13:02 -0300

    Practicing

--- src/main/SimpleCalculate.java
+++ src/main/SimpleCalculate.java
@@ -1,9 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 public class SimpleCalculate {
 
 	public static void main(String[] args) {
 
+		Scanner sr = new Scanner(System.in);
+
+		double totalAmount = 0;
+
+		for (int i = 0; i < 2; i++) {
+			// int productCode = sr.nextInt();
+			int numberOfUnits = sr.nextInt();
+			double priceUnit = sr.nextDouble();
+
+			totalAmount += numberOfUnits * priceUnit;
+		}
+
+		System.out.printf("VALOR A PAGAR: R$ %.2f%n", totalAmount);
+
+		sr.close();
 	}
 
 }
