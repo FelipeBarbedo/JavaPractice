36e00db0df2081d186dc4e89ac6fdf6ca0f06661
Felipe Barbedo <felipebarbedo54@gmail.com>
2023-12-01 06:53:45 -0300

    Practicing

--- src/main/Interval.java
+++ src/main/Interval.java
@@ -7,6 +7,31 @@ public class Interval {
 	public static void main(String[] args) {
 		Scanner sr = new Scanner(System.in);
 
+		int[] intervals = { 0, 25, 50, 75, 100 };
+		int greaterThan = -1, lessThan = -1;
+
+		double number = sr.nextDouble();
+
+		for (int i = 0; i < 5; i++)
+			if (number > intervals[i]) {
+				greaterThan = intervals[i];
+			} else {
+				lessThan = intervals[i];
+				break;
+			}
+
+		if (greaterThan == -1 || lessThan == -1)
+			System.out.println("Fora de interavalo");
+		else
+			for (int i = 0; i < 5; i++)
+				if (number == intervals[i]) {
+					System.out.printf("Intervalo (");
+				} else {
+					lessThan = intervals[i];
+					break;
+				}
+		
+		
 		sr.close();
 
 	}
